f6da8dfe5be76e11d02fcf6bf1fdd6a45d39aace
oscarlucian <menotme98@gmail.com>
2025-04-24 12:00:01 +0800

    Add

--- js/main.js
+++ js/main.js
@@ -15,12 +15,12 @@ let hover_box = document.getElementById("hover_box");
 
 //home page
 if (linkLocation.endsWith("/a/index.html") || linkLocation.endsWith("/a/")) {
-    hover_box.className = "absolute h-full w-18 bg-[#292d38] rounded-xl z-0 -translate-x-2 -translate-y-1";
+    hover_box.className = "absolute h-full w-13 md:w-18 bg-[#292d38] rounded-xl z-0 -translate-x-2 -translate-y-1";
     document.querySelector("#textColorHome").classList.add("text-[#ccc]");
 
 }//discover page
 else if (linkLocation.endsWith("/a/discover.html")) {
-    hover_box.className = "absolute h-full left-[19.5%] w-21 bg-[#292d38] rounded-xl z-0 -translate-x-2 -translate-y-1";
+    hover_box.className = "absolute h-full left-[16%] md:left-[19%] w-18 md:w-21 bg-[#292d38] rounded-xl z-0 -translate-x-2 -translate-y-1";
     document.querySelector("#textColorDis").classList.add("text-[#ccc]");
 }
 
